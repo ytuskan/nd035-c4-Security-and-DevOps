b625f1b7c032678bffbfc186d6a02320728d5018
ytuskan <tuskanyakup@gmail.com>
2021-09-22 10:51:09 +0300

    second lesson

---
+++ starter_code/src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -0,0 +1,92 @@
+package com.example.demo.controllers;
+
+import com.example.demo.TestUtils;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class UserControllerTest {
+    private UserController userController;
+
+    private UserRepository userRepository = mock(UserRepository.class);
+
+    private CartRepository cartRepository = mock(CartRepository.class);
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder = mock(BCryptPasswordEncoder.class);
+
+    @Before
+    public void setup(){
+        userController = new UserController();
+        TestUtils.injectObjects(userController, "userRepository", userRepository);
+        TestUtils.injectObjects(userController, "cartRepository", cartRepository);
+        TestUtils.injectObjects(userController, "bCryptPasswordEncoder", bCryptPasswordEncoder);
+    }
+
+    @Test
+    public void create_user_happy_path() throws Exception{
+        when(bCryptPasswordEncoder.encode("testPassword")).thenReturn("thisIsHashed");
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+        createUserRequest.setUsername("test");
+        createUserRequest.setPassword("testPassword");
+        createUserRequest.setConfirmPassword("testPassword");
+
+        final ResponseEntity<User> response = userController.createUser(createUserRequest);
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+        User user = response.getBody();
+        assertNotNull(user);
+        assertEquals(user.getId(), 0);
+        assertEquals("test", user.getUsername());
+        assertEquals("thisIsHashed", user.getPassword());
+    }
+
+    @Test
+    public void find_by_username_happy_path() throws Exception{
+
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+        createUserRequest.setUsername("test");
+        createUserRequest.setPassword("testPassword");
+        createUserRequest.setConfirmPassword("testPassword");
+
+        final ResponseEntity<User> responseUser = userController.createUser(createUserRequest);
+        User user = responseUser.getBody();
+
+        when(userRepository.findByUsername(user.getUsername())).thenReturn(user);
+
+        final ResponseEntity<User> response = userController.findByUserName(user.getUsername());
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals("test", user.getUsername());
+    }
+
+    @Test
+    public void find_by_id_happy_path() throws Exception{
+
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+        createUserRequest.setUsername("test");
+        createUserRequest.setPassword("testPassword");
+        createUserRequest.setConfirmPassword("testPassword");
+
+        final ResponseEntity<User> responseUser = userController.createUser(createUserRequest);
+        User user = responseUser.getBody();
+
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+
+        final ResponseEntity<User> response = userController.findById(user.getId());
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(0, user.getId());
+    }
+}
