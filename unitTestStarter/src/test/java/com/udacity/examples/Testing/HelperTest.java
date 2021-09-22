b4bf26db11368d65ed887460389d318443cb7885
ytuskan <tuskanyakup@gmail.com>
2021-09-22 07:44:39 +0300

    second lesson

--- unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
+++ unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
@@ -1,8 +1,14 @@
 package com.udacity.examples.Testing;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class HelperTest {
 
-	
+	@Test
+    public void test(){
+        assertEquals("", "");
+    }
 }
